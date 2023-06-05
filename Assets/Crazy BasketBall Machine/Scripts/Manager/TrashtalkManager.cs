7e5ce04bc9f2c289ee48ee9db7f7e0466c008b7e
Rey_Eclair <rey@shoor.com.ph>
2023-06-05 16:42:38 +0800

    Tweaked TrashtalkManager

--- Assets/Crazy BasketBall Machine/Scripts/Manager/TrashtalkManager.cs
+++ Assets/Crazy BasketBall Machine/Scripts/Manager/TrashtalkManager.cs
@@ -14,10 +14,9 @@ public class TrashtalkManager : MonoBehaviour
     }
 
     [SerializeField] private List<StageClass> m_StageList;
-    [SerializeField] private int m_CurrentStage; // find a way to know the stage in GameMgr
     [SerializeField] private int m_CurrentVideo; // A video to set in the VideoPlayers
     [SerializeField] VideoPlayer[] m_VideoPlayer;
-    [SerializeField] GameMgr m_GameMgr;
+    [SerializeField] GameMgr m_GameMgr; // Game Manager
     // Start is called before the first frame update
     void Start()
     {
@@ -35,11 +34,10 @@ public class TrashtalkManager : MonoBehaviour
 
     public void PlayRandomTrashtalk()
     {
-        m_CurrentStage = m_GameMgr.currentlevel;
-        m_CurrentVideo = Random.Range(0, m_StageList[m_CurrentStage].m_VideoList.Count); //Randomize to get the trashtalk
+        m_CurrentVideo = Random.Range(0, m_StageList[m_GameMgr.currentlevel].m_VideoList.Count); //Randomize to get the CurrentVideo Trashtalk
         for (int i = 0; i < m_VideoPlayer.Length; i++) //Check the number of VideoPlayer
         {
-            m_VideoPlayer[i].clip = m_StageList[m_CurrentStage].m_VideoList[m_CurrentVideo]; // set the Clips of the videoplayer depending on the current stage of the player
+            m_VideoPlayer[i].clip = m_StageList[m_GameMgr.currentlevel].m_VideoList[m_CurrentVideo]; // set the Clips of the videoplayer depending on the current stage of the player
             m_VideoPlayer[i].Play();
         }
     }
