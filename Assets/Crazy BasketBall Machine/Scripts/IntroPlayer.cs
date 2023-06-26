58678de95e861c9441af265e28cc75e4df4957a1
Rey_Eclair <rey@shoor.com.ph>
2023-06-26 13:35:27 +0800

    Removed Unused scenes & added Disclaimer

--- Assets/Crazy BasketBall Machine/Scripts/IntroPlayer.cs
+++ Assets/Crazy BasketBall Machine/Scripts/IntroPlayer.cs
@@ -1,24 +1,41 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class IntroPlayer : MonoBehaviour
 {
-    [SerializeField] VideoPlayer m_VideoPlayer;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private VideoPlayer m_VideoPlayer;
+    [SerializeField] private GameObject m_Disclaimer;
+    [SerializeField] private float m_FadeDuration = 1f;
+
+    private void Start()
     {
         // Subscribe to the loopPointReached event
         m_VideoPlayer.loopPointReached += OnVideoFinished;
-
     }
 
-
-    // Event handler for the loopPointReached event
-    void OnVideoFinished(VideoPlayer videoPlayer)
+    private void OnVideoFinished(VideoPlayer videoPlayer)
     {
+        StartCoroutine(ShowDisclaimerAndLoadScene());
+    }
+
+    private IEnumerator ShowDisclaimerAndLoadScene()
+    {
+        // Fade in the disclaimer
+        m_Disclaimer.SetActive(true);
+        m_Disclaimer.GetComponent<CanvasGroup>().alpha = 0f;
+        yield return m_Disclaimer.GetComponent<CanvasGroup>().DOFade(1f, m_FadeDuration).WaitForCompletion();
+
+        // Wait for 4 seconds
+        yield return new WaitForSeconds(4f);
+
+        // Fade out the disclaimer
+        yield return m_Disclaimer.GetComponent<CanvasGroup>().DOFade(0f, m_FadeDuration).WaitForCompletion();
+        m_Disclaimer.SetActive(false);
+
+        // Load a different scene
         SceneManager.LoadScene(1);
     }
 }
